2dcd742f0ba1f1e2d41baf08fd98cb3d6104cc78
中村文彌 <nfphys@fnakamura.local>
2022-06-23 17:18:41 +0900

    データベースを導入

--- lib/stamp.rb
+++ lib/stamp.rb
@@ -1,19 +1,67 @@
+require 'mysql2'
 require 'curses'
 require 'timeout'
 require_relative './worker.rb'
 
+datetime_regexp = /\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}/
+
+client = Mysql2::Client.new(
+  host: 'localhost',
+  username: 'root',
+  password: 'Sql0463'
+)
+client.query("use stamp_app_on_terminal;")
+
 Curses.init_screen 
-Curses.curs_set(0)
+Curses.curs_set(0) # invisible cursor
 
 begin 
   win = Curses.stdscr
 
+  # 名前の入力
   win.setpos(0, 0)
   win.addstr("名前を入力してください\n")
   win.addstr("名前: ")
   name = win.getstr
 
-  worker = Worker.new(0, name)
+  # ユーザを検索
+  users_results = client.query(
+    <<~TEXT
+    SELECT * 
+    FROM users
+    WHERE name = "#{name}"
+    ;
+    TEXT
+  )
+  if users_results.size.zero?
+    win.addstr("ユーザが見つかりませんでした")
+    win.getch
+    Curses.close_screen
+    exit
+  end
+  # p users_results.first 
+  # win.getch
+
+  user_id = users_results.first['id']
+  worker = Worker.new(user_id, name)
+
+  # 最新の勤務データを検索
+  work_data_results = client.query(
+    <<~TEXT 
+    SELECT * 
+    FROM work_data
+    WHERE user_id = '#{user_id}'
+    ORDER BY started_work_at DESC
+    LIMIT 1
+    ;
+    TEXT
+  )
+  # p work_data_results.first
+  # win.getch
+
+  if work_data_results.size.positive? && work_data_results.first['finished_work_at'].nil?
+    worker = Worker.new(user_id, name, work_data_results.first['started_work_at'])
+  end
   
   log = ""
 
@@ -43,7 +91,7 @@ begin
       Timeout.timeout(0.1) do
         operation_number = win.getch.to_i 
         case operation_number 
-        when 1
+        when 1 
           if worker.finished_work_at 
             log = "既に退勤しています"
             break 
@@ -54,6 +102,18 @@ begin
           end
           worker = worker.start_work 
           log = "出勤しました"
+
+          # データベースに出勤時間を記録
+          client.query(
+            <<~TEXT
+            INSERT INTO work_data(started_work_at, user_id) 
+            VALUES (
+              '#{worker.started_work_at.to_s.scan(datetime_regexp).first}', 
+              '#{worker.id}'
+            )
+            TEXT
+          )
+
         when 2 
           if worker.started_work_at.nil?
             log = "まだ出勤していません"
@@ -65,12 +125,31 @@ begin
           end
           worker = worker.finish_work 
           log = "退勤しました"
+
+          # データベースに退勤時間を記録
+          client.query(
+            <<~TEXT
+            UPDATE work_data
+            SET finished_work_at = '#{worker.finished_work_at.to_s.scan(datetime_regexp).first}'
+            WHERE started_work_at = '#{worker.started_work_at.to_s.scan(datetime_regexp).first}'
+            TEXT
+          )
+
         when 3 
+          client.query(
+            <<~TEXT
+            DELETE FROM work_data
+            WHERE started_work_at = '#{worker.started_work_at.to_s.scan(datetime_regexp).first}'
+            TEXT
+          )
+
           worker = worker.reset 
           log = "リセットしました"
+
         when 4 
           Curses.close_screen
           exit
+
         end
       end
     rescue
