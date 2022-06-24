20d3db8839c1ca8f4d38c7863423106e63428d3e
中村文彌 <nfphys@fnakamura.local>
2022-06-24 16:29:22 +0900

    インスタンス変数started_break_atとfinished_break_atを追加し、その値をチェックするメソッドを追加した

--- lib/worker.rb
+++ lib/worker.rb
@@ -3,7 +3,7 @@ require 'time'
 require_relative './timer.rb'
 
 class Worker
-  attr_reader :id, :name, :started_work_at, :finished_work_at
+  attr_reader :id, :name, :started_work_at, :finished_work_at, :started_break_at, :finished_break_at
 
   def self.check_id(id)
     unless id.instance_of?(Integer)
@@ -21,15 +21,19 @@ class Worker
     end
   end
 
-  def self.check_work_time(started_work_at, finished_work_at)
+  def self.check_started_work_at(started_work_at)
     unless started_work_at.nil? || started_work_at.instance_of?(Time)
       raise TypeError, 'started_work_at must be nil or an instance of Time.'
     end
+  end
 
+  def self.check_finished_work_at(finished_work_at)
     unless finished_work_at.nil? || finished_work_at.instance_of?(Time)
       raise TypeError, 'finished_work_at must be nil or an instance of Time.'
     end
+  end
 
+  def self.check_started_and_finished_work_at(started_work_at, finished_work_at)
     if started_work_at.nil? && !finished_work_at.nil?
       raise ArgumentError, 'finished_work_at must be nil when started_work_at is nil.'
     end
@@ -39,15 +43,88 @@ class Worker
     end
   end
 
-  def initialize(id, name, started_work_at=nil, finished_work_at=nil)
+  def self.check_started_break_at(started_work_at, finished_work_at, started_break_at)
+    unless started_break_at.instance_of?(Array)
+      raise TypeError, 'started_break_at must be an array.'
+    end
+
+    started_break_at.each do |e|
+      unless e.instance_of?(Time)
+        raise TypeError, 'Each element of started_break_at must be an instance of Time.'
+      end
+
+      if e < started_work_at 
+        raise ArgumentError, 'Each element of started_break_at must be after started_work_at.'
+      end
+
+      if !finished_work_at.nil? && e > finished_work_at 
+        raise ArgumentError, 'Each element of started_break_at must be before finished_work_at.'
+      end
+    end
+
+    (0...(started_break_at.size - 1)).each do |i|
+      unless started_break_at[i] < started_break_at[i+1]
+        raise ArgumentError, 'started_break_at[i] must be before started_break_at[i+1].'
+      end
+    end
+  end
+
+  def self.check_finished_break_at(started_work_at, finished_work_at, finished_break_at)
+    unless finished_break_at.instance_of?(Array)
+      raise TypeError, 'finished_break_at must be an array.'
+    end
+
+    finished_break_at.each do |e|
+      unless e.instance_of?(Time)
+        raise TypeError, 'Each element of finished_break_at must be an instance of Time.'
+      end
+
+      if e < started_work_at
+        raise ArgumentError, 'Each element of finished_break_at must be after started_work_at.'
+      end
+
+      if !finished_work_at.nil? && e > finished_work_at
+        raise ArgumentError, 'Each element of finished_break_at must be before finished_work_at'
+      end
+    end
+
+    (0...(finished_break_at.size - 1)).each do |i|
+      unless finished_break_at[i] < finished_break_at[i+1]
+        raise ArgumentError, 'finished_break_at[i] must be before finished_break_at[i].'
+      end
+    end
+  end
+
+  def self.check_started_and_finished_break_at(started_break_at, finished_break_at)
+    unless (started_break_at.size - finished_break_at.size).between?(0, 1)
+      raise ArgumentError, '(started_break_at.size - finished_break_at.size) must be 0 or 1.'
+    end
+
+    (0...started_break_at.size).each do |i|
+      if !finished_break_at[i].nil? && started_break_at[i] > finished_break_at[i]
+        raise ArgumentError, 'started_break_at[i] must be before finished_break_at[i].'
+      end
+    end
+  end
+
+  def initialize(id, name, started_work_at=nil, finished_work_at=nil, started_break_at=[], finished_break_at=[])
     Worker.check_id(id)
     Worker.check_name(name)
-    Worker.check_work_time(started_work_at, finished_work_at)
+
+    Worker.check_started_work_at(started_work_at)
+    Worker.check_finished_work_at(finished_work_at)
+    Worker.check_started_and_finished_work_at(started_work_at, finished_work_at)
+
+    Worker.check_started_break_at(started_work_at, finished_work_at, started_break_at)
+    Worker.check_finished_break_at(started_work_at, finished_work_at, finished_break_at)
+    Worker.check_started_and_finished_break_at(started_break_at, finished_break_at)
 
     @id = id
     @name = name 
     @started_work_at = started_work_at
     @finished_work_at = finished_work_at
+    @started_break_at = started_break_at
+    @finished_break_at = finished_break_at
     self.freeze
   end
 
