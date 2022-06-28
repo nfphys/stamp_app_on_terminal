002f5414ef24559c82002c4e5ffc83c82197a671
中村文彌 <nfphys@fnakamura.local>
2022-06-28 21:30:59 +0900

    create_databaseを追加

--- lib/stamp.rb
+++ lib/stamp.rb
@@ -3,12 +3,12 @@ require 'curses'
 require 'timeout'
 require_relative './stamp/worker.rb'
 
-def stamp 
+def stamp(host: 'localhost', username: 'root', password: )
   client = 
     Mysql2::Client.new(
-      host: 'localhost',
-      username: 'root',
-      password: 'Sql0463'
+      host: host,
+      username: username,
+      password: password
     )
 
   Curses.init_screen 
@@ -122,4 +122,77 @@ def stamp
   rescue
     Curses.close_screen
   end
+end
+
+def create_database(host: 'localhost', username: 'root', password: )
+  client = 
+    Mysql2::Client.new(
+      host: host,
+      username: username,
+      password: password
+    )
+
+  begin 
+    client.query('CREATE DATABASE stamp_app_on_terminal')
+  rescue Mysql2::Error => e 
+    puts e.message 
+  end
+
+  client.query('USE stamp_app_on_terminal')
+
+  begin 
+    client.query(
+      <<~TEXT
+      CREATE TABLE users (
+        id INT AUTO_INCREMENT, 
+        name TEXT, 
+        PRIMARY KEY (id)
+      )
+      TEXT
+    )
+  rescue Mysql2::Error => e 
+    puts e.message 
+  end
+
+  begin 
+    client.query(
+      <<~TEXT
+      CREATE TABLE work_data (
+        id INT AUTO_INCREMENT, 
+        started_work_at DATETIME, 
+        finished_work_at DATETIME, 
+        user_id INT, 
+        PRIMARY KEY (id)
+      )
+      TEXT
+    )
+  rescue Mysql2::Error => e 
+    puts e.message 
+  end
+
+  begin 
+    client.query(
+      <<~TEXT
+      CREATE TABLE break_data (
+        id INT AUTO_INCREMENT, 
+        started_break_at DATETIME, 
+        finished_break_at DATETIME, 
+        user_id INT, 
+        work_data_id INT, 
+        PRIMARY KEY (id)
+      )
+      TEXT
+    )
+  rescue Mysql2::Error => e 
+    puts e.message 
+  end
+end
+
+def delete_database(host: 'localhost', username: 'root', password: )
+  client = 
+    Mysql2::Client.new(
+      host: host,
+      username: username,
+      password: password
+    )
 end
