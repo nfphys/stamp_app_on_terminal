19071070dfc2738bf0e50fd88e8f5b28b1f4dfd5
中村文彌 <nfphys@fnakamura.local>
2022-06-29 16:06:55 +0900

    *, +, -メソッドのテストを削除

--- test/timer_test.rb
+++ test/timer_test.rb
@@ -26,24 +26,6 @@ class TimerTest < Minitest::Test
     assert Worker::Timer.new(10, 45, 22).between?(Worker::Timer.new(0, 0, 0), Worker::Timer.new(24, 0, 0))
   end
 
-  def test_add 
-    assert_equal Worker::Timer.new(2, 39, 3), Worker::Timer.new(1, 20, 0) + Worker::Timer.new(1, 19, 3)
-    assert_equal Worker::Timer.new(0, 2, 54), Worker::Timer.new(0, 1, 20) + 94
-  end
-
-  def test_subtraction 
-    assert_equal Worker::Timer.new(1, 20, 0), Worker::Timer.new(2, 39, 3) - Worker::Timer.new(1, 19, 3)
-    assert_equal Worker::Timer.new(1, 19, 3), Worker::Timer.new(2, 39, 3) - Worker::Timer.new(1, 20, 0)
-    assert_equal Worker::Timer.new(0, 0, 0), Worker::Timer.new(2, 39, 3) - Worker::Timer.new(4, 19, 8)
-    assert_equal Worker::Timer.new(0, 1, 20), Worker::Timer.new(0, 2, 54) - 94
-  end
-
-  def test_muliplication
-    assert_equal Worker::Timer.new(0, 1, 30), Worker::Timer.new(0, 0, 30) * 3
-    assert_equal Worker::Timer.new(6, 17, 6), Worker::Timer.new(2, 5, 42) * 3
-    assert_equal Worker::Timer.new(94, 29, 0), Worker::Timer.new(1, 34, 29) * 60
-  end
-
   def test_to_s 
     assert_equal '00:00:00', Worker::Timer.new(0, 0, 0).to_s
     assert_equal '02:24:05', Worker::Timer.new(2, 24, 5).to_s
